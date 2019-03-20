2b2a09a9bc7eb9d3cb80b49d2faefe20a7722b84
Bryan Huang <bhuang453@yahoo.com>
2019-03-20 13:01:32 -0700

    Update Time.java

--- Lab30_Time/src/Time.java
+++ Lab30_Time/src/Time.java
@@ -5,6 +5,7 @@
  */
 
 public class Time {
+	
 	//Fields:
 	private int hours;
 	private int minutes;
@@ -12,12 +13,13 @@ public class Time {
 	
 	//Constructors:
 	public Time() {
-		hours = 0;
-		minutes = 0;
+		this.hours = 0;
+		this.minutes = 0;
 	}
 	public Time(int hours, int minutes) {
 		if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
-			throw new IllegalArgumentException("Time must be in a valid time range!");
+			//throw new IllegalArgumentException("Time must be in a valid time range!");
+			System.out.println("Time must be in a valid time range!");
 		}
 		this.hours = hours;
 		this.minutes = minutes;
@@ -27,14 +29,31 @@ public class Time {
 	
 	//Methods:
 	public String toString() {
-		String strHour = hours + "";
-		String strMinute = minutes + "";
+//		String strHour = hours + "";
+//		String strMinute = minutes + "";
+//		
+//		if (hours < 10) strHour = "0" + strHour;
+//		if (minutes < 10) strHour = "0" + strHour;
+//		
+//		time = strHour + ":" + strMinute;
+//		
+//		return time;
 		
-		if (hours < 10) strHour = "0" + strHour;
-		if (minutes < 10) strHour = "0" + strHour;
+		String minSpace = "" + minutes;
+		String strHours = "" + hours;
 		
-		time = strHour + ":" + strMinute;
+		//System.out.println(minSpace);
 		
-		return time;
+		if(minSpace.length() == 1) {
+			minSpace = "0" + minSpace;
+		}
+		
+		if(strHours.length() == 1) {
+			strHours = "0" + strHours;
+		}
+		
+		//System.out.println(minSpace);
+		
+		return strHours + ":" + minSpace;
 	}
 }
