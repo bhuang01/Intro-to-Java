6c51e14c9f535ef5bc3380b9ad1f2ea3a9b551e9
Bryan Huang <bhuang453@yahoo.com>
2019-03-22 13:04:02 -0700

    Update TimeTest.java

--- Lab30_Time/src/TimeTest.java
+++ Lab30_Time/src/TimeTest.java
@@ -14,5 +14,10 @@ public class TimeTest {
 		
 		System.out.println(t1 + " " + t2 + " " + t3 + " " + t4);
 		
+		//System.out.println(t3.toMins()); //Testing the toMins() public method
+		
+		//System.out.println(t1.elapsedSince(t2)); //Testing the elapsedTime method
+	
+		
 	}
 }
